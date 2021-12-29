5b537ff3878396159ff8a45edbd7ce0a641ddf7b
Deepika-abc <96698769+Deepika-abc@users.noreply.github.com>
2021-12-30 01:10:05 +0530

    Update firstinstance.tf

--- firstinstance.tf
+++ firstinstance.tf
@@ -9,7 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "us-west-2"
- 
+ access_key = "AKIA26ZDL6DTF7UQ5UJS"
+  secret_key = "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"
 }
 resource "aws_instance" "myec2" {
   ami = "ami-00f7e5c52c0f43726"
